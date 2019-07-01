c2db3d97bea619b7cb288fe09aefb34d59b42646
Li <liyp0095@atan-115b-11.cs.iastate.edu>
2019-06-30 23:39:14 -0500

    passcal triangle

--- learnPython/scripts/test.py
+++ learnPython/scripts/test.py
@@ -8,9 +8,37 @@ L2 = [word.lower() for word in L1 if type(word) == type("")]
 print(L2)
 
 
-def pascal_triangle(max):
-    n, a, b = 0, [], []
-    while n < max:
-        yield b
-        a = b
-        b = []
+import itertools
+
+def pairwise(iterable):
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def double_chuncker(iterable):
+    extended = itertools.chain([0], iterable, [0])
+    return pairwise(extended)
+
+
+def pascal_next(iterable):
+    return list(map(sum, double_chuncker(iterable)))
+
+
+def pascal_triangle():
+    row = [1]
+    while True:
+        yield row
+        row = pascal_next(row)
+
+
+def pascal_triangle_up_to(n):
+    return list(itertools.islice(pascal_triangle(), n))
+
+
+if __name__ == '__main__':
+    # Testing
+    for row in pascal_triangle():
+        print(row, end='')
+        if (input()):
+            break
